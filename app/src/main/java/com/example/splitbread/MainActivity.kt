e17c200a2d1c1d3ac59fa7eba8b512d12dd539e3
Abubakr2000 <Abubakr.Alijanov@rockets.utoledo.edu>
2022-04-23 02:35:06 -0400

    Edited design, committed main activity

--- app/src/main/java/com/example/splitbread/MainActivity.kt
+++ app/src/main/java/com/example/splitbread/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.splitbread
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_friends)
 
+        val btnAddFriends = findViewById<Button>(R.id.addFriends)
+
+        btnAddFriends.setOnClickListener{
+            startActivity(Intent(this,AddFriendsActivity::class.java))
+        }
 
 
     }
